a170dda7276551dfb77d3cf20b73bfc30a2c57da
jjl01 <329926193@qq.com>
2018-11-07 10:37:37 +0800

    添加物流单修改

--- kdi-bll/src/main/java/rebue/kdi/svc/impl/KdiLogisticSvcImpl.java
+++ kdi-bll/src/main/java/rebue/kdi/svc/impl/KdiLogisticSvcImpl.java
@@ -195,13 +195,7 @@ public class KdiLogisticSvcImpl extends MybatisBaseSvcImpl<KdiLogisticMo, java.l
         kdiLogisticMo.setOrderId(_idWorker.getId());
         kdiLogisticMo.setOrderTime(now);
         kdiLogisticMo.setUpdateTime(now);
-//        _log.info("录入订单的请求参数为: {}", mo);
-//        int result = add(kdiLogisticMo);
-//        if (result != 1) {
-//            return -1;
-//        }
-        _log.info("录入订单成功, {}", now);
-        _log.info("订阅订单物流轨迹");
+        _log.info("订阅订单物流轨迹并添加物流单");
 		SubscribeTraceTo subscribeTraceTo = dozerMapper.map(kdiLogisticMo, SubscribeTraceTo.class);
 		_log.info("订阅订单物流轨迹参数为: {}", subscribeTraceTo);
 		SubscribeTraceRo subscribeTraceRo = kdiSvc.subscribeTrace(subscribeTraceTo);

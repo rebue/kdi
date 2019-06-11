f3d66d142ea9b561c604628b38e877732c77a554
100111100 <253274870@qq.com>
2019-06-11 12:19:24 +0800

    更新添加字段语句到线上

--- db/script/update.sql
+++ db/script/update.sql
@@ -42,3 +42,14 @@ create table KDI_TEMPLATE_DIC
 
 alter table KDI_TEMPLATE_DIC comment '模板字典';
 -- ---------------------------- 上面的已经更新到大卖网线上 ----------------------------
+
+alter table KDI_COMPANY add  SHOP_ID       bigint comment '店铺ID，用来表示这个快递公司是那个店铺的默认快递公司';
+alter table KDI_COMPANY add  SHOP_NAME        varchar(50) comment '默认使用该快递公司的店铺';
+
+alter table KDI_SENDER add SHOP_ID              bigint comment '店铺ID，用来表示这个发件人是那个店铺的默认发件人';
+alter table KDI_SENDER add SHOP_NAME            varchar(50) comment '默认使用该发件人的店铺';
+
+alter table KDI_COMPANY add  ANOTHER_NAME         varchar(50) comment '快递公司别名，用于区分一个买家的两个店铺使用同一个快递公司';
+
+-- ---------------------------- 上面的已经更新到大卖网线上2019-06-11 ----------------------------
+
